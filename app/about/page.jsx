34d2c88d44cc3acf64efa7fec5856d0192bf02f2
Mukesh Kr. Gupta <rg37090@gmail.com>
2024-01-31 17:29:26 +0530

    adding css

---
+++ app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="pt-16 text-4xl">ABOUT US</h1>
+      <p className="pt-16 mx-64">
+        Welcome to our Blog App! We are passionate about creating and sharing
+        valuable content with our community. Here's a bit about us: Our Mission:
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in leo
+        sit amet odio ultricies feugiat. Quisque tincidunt bibendum risus, non
+        feugiat felis ultricies id. Our Team: Meet the dedicated individuals
+        behind the scenes who work tirelessly to bring you engaging and
+        informative blog posts. - John Doe (Founder and CEO): Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Sed eleifend orci eu quam
+        ultrices, vel varius neque tincidunt. - Jane Smith (Content Editor):
+        Vestibulum id quam id nisi convallis finibus. Phasellus vel justo nec
+        arcu tincidunt dignissim. Get in Touch: Have questions, suggestions, or
+        just want to say hello? Contact us at info@blogapp.com. Thank you for
+        being a part of our community! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laborum veniam error aliquid sint possimus eaque
+        voluptates illum laudantium eius sed dolores corporis repellat amet,
+        odio, debitis nulla numquam reprehenderit fugiat! Lorem ipsum dolor, sit
+        amet consectetur adipisicing elit. Dolorum perferendis totam
+        voluptatum quia accusantium quasi blanditiis, vero tempore repudiandae,
+        numquam reprehenderit repellat, voluptas ad atque distinctio ipsa!
+        Exercitationem, eligendi fugit.
+      </p>
+    </div>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="pt-12 text-4xl">CONTACT US</h1>
+      <p className="pt-16 mx-8 lg:mx-32 xl:mx-64">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi,
+        nesciunt fuga ratione sapiente qui, dolorem est sint dicta adipisci
+        expedita quasi recusandae soluta veniam nulla assumenda tenetur
+        laboriosam repudiandae a aliquam illum! Non impedit, velit quisquam
+        commodi amet vel nulla qui, quasi ratione reiciendis ex. Consectetur,
+        necessitatibus illo! Ipsam aspernatur eos ex? Autem doloremque aliquid
+        quasi reiciendis dolorum, magni asperiores nobis, iure, culpa officia
+        sit numquam sed provident nostrum ea maxime eius inventore non maiores
+        unde. Laborum, eius repudiandae maxime corrupti officiis suscipit hic,
+        cupiditate eaque ab ullam impedit. Qui ratione facilis non laboriosam
+        repellendus consequatur itaque reiciendis nisi officiis. Amet
+        accusantium repudiandae aut laboriosam autem unde sint reiciendis!
+        Praesentium dignissimos, voluptas commodi, magnam voluptate sit in
+        perferendis vel tempora neque laboriosam illum? Voluptatum nesciunt
+        dicta quibusdam, nihil, odit repudiandae iusto veniam accusantium
+        repellendus eaque dolor consectetur? Sed dolorum quidem dolores
+        consequatur, placeat velit temporibus aliquid veritatis tempora
+        consequuntur, ipsum fugit magni autem quae commodi? Culpa quibusdam
+        provident molestias commodi soluta illo explicabo, consequatur rerum
+        suscipit? Quisquam voluptatibus odio magnam, commodi inventore eius esse
+        modi ipsa doloremque dolore! Consectetur eius voluptatem molestias vitae
+        dolore? Quisquam cupiditate pariatur vitae, neque ipsam molestias ex
+        quibusdam suscipit eveniet autem quidem eos unde quas voluptate quasi
+        harum dicta dolorum molestiae perspiciatis? Esse sint consequatur, vitae
+        fugiat corporis vero, facilis nisi totam natus animi cumque autem minima.
+      </p>
+    </div>
+  );
+}
